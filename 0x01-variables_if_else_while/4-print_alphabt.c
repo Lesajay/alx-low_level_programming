53ea0d57b2ad9b8e56fd9d9c7cf0ec7290d4ad7d
Lesajay <craigmazwi@gmail.com>
2022-09-09 09:15:05 -0700

    last

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@ int main(void)
 
 	l = 'a';
 
-	while (l < 'z')
+	while (l <= 'z')
 	{
 		if (l != 'e' && l != 'q')
 			putchar(l);
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int n;
+
+
+
+	for (n = 48; n < 58; n++)
+
+	{
+
+		putchar(n);
+	}
+
+	putchar('\n');
+	return (0);
+}
