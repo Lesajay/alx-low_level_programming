dd9fa4eda8483953ba3997c29343dce77eb27835
Lesajay <craigmazwi@gmail.com>
2022-09-13 11:34:56 -0700

    second

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints aphabet in lowcase
+ * Return: Nothing
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i)
+	_putchar('\n')
+}
