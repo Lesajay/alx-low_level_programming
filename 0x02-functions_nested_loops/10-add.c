b1524f295f1e2192f803a001b7cebbf5b9c22a7e
Lesajay <craigmazwi@gmail.com>
2022-09-13 15:42:45 -0700

    lastt

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - Check
+ * @a: An integer a
+ * @b: An integer b
+ * Description: Function that adds two integers
+ * Return: Result to add a and b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+
+}
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - checks code
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}
